932d56e5057d8b0f8647d5adff6fd6554cf7d916
aman joshi <amanjoshi0120@gmail.com>
2020-08-24 14:25:33 +0530

    Updates

---
+++ precache-manifest.f88e24ccf1fe274064c5844826b3ec0d.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dbef207dbf0f89049aabb24e9fd7e40",
+    "url": "/Jugadu_react/index.html"
+  },
+  {
+    "revision": "1b6879f8a5d79eaa3d65",
+    "url": "/Jugadu_react/static/css/2.ceb79488.chunk.css"
+  },
+  {
+    "revision": "7095cc6c1ea5b5f58d25",
+    "url": "/Jugadu_react/static/css/main.cd5e7050.chunk.css"
+  },
+  {
+    "revision": "1b6879f8a5d79eaa3d65",
+    "url": "/Jugadu_react/static/js/2.7fd37cd7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Jugadu_react/static/js/2.7fd37cd7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7095cc6c1ea5b5f58d25",
+    "url": "/Jugadu_react/static/js/main.c84af657.chunk.js"
+  },
+  {
+    "revision": "4b7a75a49beb8026b85f",
+    "url": "/Jugadu_react/static/js/runtime-main.f8ad94fc.js"
+  },
+  {
+    "revision": "d3d8c1efe351a4dd36e65d258d4d9633",
+    "url": "/Jugadu_react/static/media/BG_right.d3d8c1ef.png"
+  },
+  {
+    "revision": "2e3d103013839ece29736008f4881560",
+    "url": "/Jugadu_react/static/media/aditya_rastogi.2e3d1030.jpg"
+  },
+  {
+    "revision": "1908b735b4d94d3a4899d1576941345d",
+    "url": "/Jugadu_react/static/media/aman_kumar.1908b735.jpg"
+  },
+  {
+    "revision": "31973cd51d395dbb326594eb875a4f5a",
+    "url": "/Jugadu_react/static/media/ankita_bose.31973cd5.jpg"
+  },
+  {
+    "revision": "133967c4d8901896ebac2de54f09ce02",
+    "url": "/Jugadu_react/static/media/app.133967c4.svg"
+  },
+  {
+    "revision": "0bfefe363fcb2a1c182f4b660cb28c13",
+    "url": "/Jugadu_react/static/media/branding.0bfefe36.svg"
+  },
+  {
+    "revision": "a87f1da48cf8d58cc4d68011d594b15c",
+    "url": "/Jugadu_react/static/media/dhruv_saharan.a87f1da4.jpeg"
+  },
+  {
+    "revision": "d3c05091f08fd2ffdd0e04448aa0141f",
+    "url": "/Jugadu_react/static/media/digital.d3c05091.svg"
+  },
+  {
+    "revision": "077c941105458b5b85c0520e747fafa4",
+    "url": "/Jugadu_react/static/media/footer_bg.077c9411.png"
+  },
+  {
+    "revision": "ffd312b4ec9c57c8a175b146e9a3718e",
+    "url": "/Jugadu_react/static/media/gunjan.ffd312b4.jpg"
+  },
+  {
+    "revision": "89be9eb8160505eaa5988f2cd5c229b4",
+    "url": "/Jugadu_react/static/media/jatin_pathi.89be9eb8.jpg"
+  },
+  {
+    "revision": "ef5626ed405644ee0e362aca93f5bf64",
+    "url": "/Jugadu_react/static/media/logo.ef5626ed.png"
+  },
+  {
+    "revision": "8fdd037acd49b5b9be4b8d15afa9948f",
+    "url": "/Jugadu_react/static/media/next.8fdd037a.svg"
+  },
+  {
+    "revision": "6c30be79b0d0bddc83ee0367a56949d4",
+    "url": "/Jugadu_react/static/media/nikhil_mawar.6c30be79.jpg"
+  },
+  {
+    "revision": "b1dce8d25cee3f2b06e615c4722e5135",
+    "url": "/Jugadu_react/static/media/plus.b1dce8d2.svg"
+  },
+  {
+    "revision": "25549b18ccce2d7eb9564b58530d158b",
+    "url": "/Jugadu_react/static/media/raghav_pahwa.25549b18.jpg"
+  },
+  {
+    "revision": "40fe3008dc2c325bfab9b4bb2bc93f0f",
+    "url": "/Jugadu_react/static/media/research.40fe3008.svg"
+  },
+  {
+    "revision": "aa6f8c1a2cd0bcbf49e82ccfffb16933",
+    "url": "/Jugadu_react/static/media/seo.aa6f8c1a.svg"
+  },
+  {
+    "revision": "fc8afc8a3d995b0bedfc18ceb3a2c1ac",
+    "url": "/Jugadu_react/static/media/simerneet.fc8afc8a.jpg"
+  },
+  {
+    "revision": "f6bdfc9d3848ba32a2eb2642d9bfb65e",
+    "url": "/Jugadu_react/static/media/vasu_goel.f6bdfc9d.jpg"
+  },
+  {
+    "revision": "275a9da1601c3e9a73888eee08452be7",
+    "url": "/Jugadu_react/static/media/vishes_kansal.275a9da1.jpg"
+  },
+  {
+    "revision": "5397052e98abc6ddd0a1ca65e6f369a5",
+    "url": "/Jugadu_react/static/media/visit_1.5397052e.jpg"
+  },
+  {
+    "revision": "68894b73f12dbfc348e9d3fc0873be70",
+    "url": "/Jugadu_react/static/media/visit_2.68894b73.jpg"
+  },
+  {
+    "revision": "e5ff73ce04a5495785e5a8230f4661ec",
+    "url": "/Jugadu_react/static/media/visit_3.e5ff73ce.jpg"
+  },
+  {
+    "revision": "84efed6c70af8d84dcee776c404f6ddb",
+    "url": "/Jugadu_react/static/media/visit_4.84efed6c.jpg"
+  },
+  {
+    "revision": "e47d9ca92e7c813f7039505da0d59d80",
+    "url": "/Jugadu_react/static/media/we-create.e47d9ca9.png"
+  },
+  {
+    "revision": "1227f7edc0704ff99f73e0425d85ab5d",
+    "url": "/Jugadu_react/static/media/whatsapp.1227f7ed.png"
+  }
+]);

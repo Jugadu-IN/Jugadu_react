35f488993b4b74c17326c0ebf439aa6725befda8
Simerneet <saibersingh@gmail.com>
2020-11-21 00:13:26 +0530

    Added team members to our team section.

--- src/components/Teamcards.js
+++ src/components/Teamcards.js
@@ -1,5 +1,6 @@
 import simerneet from "./Ourteam/simerneet.jpg";
 import vasu from "./Ourteam/vasu_goel.jpg";
+import mohit from "./Ourteam/mohit.jpg";
 import dhruv from "./Ourteam/dhruv_saharan.jpeg";
 import jatin from "./Ourteam/jatin_pathi.jpg";
 import gunjan from "./Ourteam/gunjan.jpg";
@@ -10,13 +11,18 @@ import vishesh from "./Ourteam/vishes_kansal.jpg";
 import nikhil from "./Ourteam/nikhil_mawar.jpg";
 import aditya from "./Ourteam/aditya_rastogi.jpg";
 import amanjoshi from "./Ourteam/amanjoshi.jpg";
+import arun from "./Ourteam/Arun_H.jpg";
+import prableen from "./Ourteam/prableen.jpg";
+import georgi from "./Ourteam/georgi.jpg";
+import akshita from "./Ourteam/akshita.JPG";
+import sumit from "./Ourteam/sumit.jpg";
 const Teamcards=[{
     image:simerneet,
    name:"Simerneet Singh",
    Position:"Co-Founder",
    Location:"New Delhi,India",
    Link:"simerneet",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+   description:"Jugadu.in started as a platform to provide high quality digital solutions to business at affordable prices, but now we have grown into a platform building amazing projects and helping developers to work on real life projects to build great ideas together. I am happy to be a part on this amazing journey and I enjoy to see myself grow along with every person associated with us."
 },
 {   
     image:vasu,
@@ -24,82 +30,126 @@ const Teamcards=[{
    Position:"Co-Founder",
    Location:"New Delhi,India",
    Link:"vasu",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+   description:"I see Jugadu becoming a platform for developers who want to work on real world projects. I don't wish to run things as internships rather it would be great if it's more like a place where people work on great ideas together. As for me I think I really don't know what I want to do or what my goals are. But this stuff makes me happy and I enjoy it."
 },
-{   image:dhruv,
+{   
+    image:mohit,
+    name:"Mohit Dhattarwal",
+    Position:"Operations Head",
+    Location:"Haryana,India",
+    Link:"mohit",
+    description:"For me Jugadu.in is like a compiler, we feed in our ideas and the whole team works to execute a real world application. I believe in solving problems and that is exactly what I do here at Jugadu.in."
+},
+{   
+    image:dhruv,
     name:"Dhruv Sahnan",
    Position:"Server Side Developer",
    Location:"New Delhi,India",
    Link:"dhruv",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+   description:""
 },
 {   
     image:jatin,
-    name:"Jatin Parthi",
-    Position:"Creative Lead",
+    name:"Jatin Pathi",
+    Position:"Creative Director",
     Location:"New Delhi,India",
     Link:"jatin",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    description:"As an artist my goal is to motivate others with my artwork."
 },
-{   
-    image:gunjan,
-    name:"Gunjan",
-    Position:"Web Developer Intern",
+{
+    image:georgi,
+    name:"Georgi Dimitrov",
+    Position:"Django Developer - Intern",
+    Location:"Sofia,Bulgaria",
+    Link:"georgiD",
+    description:"I really like Jugadu as a company. The thing that I like the most is the supportive team environment. When we have a task we don't have to do it on our own and we can depend on the rest of the team for help. Also it's great that everyone is part of the team - there are no bosses, employees and interns - everyone can share their ideas and everyone can contribute equally."
+},
+{
+    image:prableen,
+    name:"Prableen Kaur",
+    Position:"Web Developer - Intern",
     Location:"New Delhi,India",
-    Link:"gunjan",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    Link:"prableen",
+    description:"I am currently a sophomore at Guru Tegh Bahadur Institute of Technology having majored in Computer Science and Engineering. I am currently in my third year. I have a tilted side towards Web development and Data Science and would love to work on projects related to these respective fields in the future."
 },
-{   image:raghav,
-    name:"Raghav Pahwa",
-    Position:"Web Developer Intern",
+{
+    image:arun,
+    name:"Arun H",
+    Position:"Django Developer - Intern",
+    Location:"Kerala,India",
+    Link:"arunH",
+    description:"I am a fourth year student at Model Engineering College, Kochi, Kerala. Django developer, loves logic, puzzles, chess and books(fiction)."
+},
+{
+    image:akshita,
+    name:"Akshita Bansal",
+    Position:"Web developer - Intern",
     Location:"New Delhi,India",
-    Link:"raghav",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},
-{   image:amankumar,
-    name:"Aman Kumar",
-    Position:"Flutter Developer Intern",
-    Location:"New Delhi,India",
-    Link:"aman",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},
-{   image:ankita,
-    name:"Ankita Bose",
-    Position:"Flutter Developer Intern",
-    Location:"West Bengal,India",
-    Link:"ankita",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},
-{   image:vishesh,
-    name:"Vishesh Kansal",
-    Position:"Web Developer Intern",
-    Location:"Gurgaon,India",
-    Link:"vishesh",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},
-{   image:nikhil,
-    name:"Nikhil Mawar",
-    Position:"UI/UX Developer Intern",
-    Location:"New Delhi,India",
-    Link:"nikhil",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-},
-{   
-    image:aditya,
-    name:"Aditya Rastogi",
-    Position:"Web Developer Intern",
-    Location:"New Delhi,India",
-    Link:"aditya",
-   description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    Link:"akshitabansal",
+    description:"Developing amazing Websites is what I do."
 },
 {
     image:amanjoshi,
     name:"Aman Joshi",
-    Position:"Web developer Intern",
+    Position:"Web developer - Intern",
     Location:"New Delhi,India",
     Link:"amanjoshi",
-    description:"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-
+    description:"Developing amazing Websites is what I do."
+},
+{   
+    image:gunjan,
+    name:"Gunjan",
+    Position:"Web Developer - Intern",
+    Location:"New Delhi,India",
+    Link:"gunjan",
+   description:"I am an optimistic and aggressive person who like to overcome challenges with the exceptional ability of learning and gaining knowledge. I have worked tirelessly on my communication skills and teamwork abilities, which I will put in use. In long term goal I want to be a full stack software developer."
+},
+{   image:raghav,
+    name:"Raghav Pahwa",
+    Position:"Web Developer - Intern",
+    Location:"New Delhi,India",
+    Link:"raghav",
+    description:"I like to solve real life problems with the power of code."
+},
+{   
+    image:amankumar,
+    name:"Aman Kumar",
+    Position:"Flutter Developer - Intern",
+    Location:"New Delhi,India",
+    Link:"amankumar",
+    description:"Being in second i wanted to explore different fields related to information technology flutter was among the first ."
+},
+{   
+    image:ankita,
+    name:"Ankita Bose",
+    Position:"Flutter Developer - Intern",
+    Location:"West Bengal,India",
+    Link:"ankita",
+    description:"Final year undergraduate CSE student."
+},
+{   
+    image:vishesh,
+    name:"Vishesh Kansal",
+    Position:"Web Developer - Intern",
+    Location:"Gurgaon,India",
+    Link:"vishesh",
+    description:"I am a Full Stack Developer who loves to code and create amazing websites."
+},
+{   
+    image:nikhil,
+    name:"Nikhil Mawar",
+    Position:"UI/UX Developer - Intern",
+    Location:"New Delhi,India",
+    Link:"nikhil",
+    description:""
+},
+{   
+    image:aditya,
+    name:"Aditya Rastogi",
+    Position:"Web Developer - Intern",
+    Location:"New Delhi,India",
+    Link:"aditya",
+    description:"A programmer by heart and soul, just need a laptop and some old songs to get going!"
 }
 ];
 export default Teamcards;
